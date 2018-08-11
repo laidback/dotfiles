297833b81513ed7c4ab6ef83a4ddb540639d5006
Lukas Ciszewski <Lukas.Ciszewski@Gmail.com>
2018-08-11 22:06:22 +0200

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -5,11 +5,11 @@ set -o errtrace
 set -o nounset
 set -o pipefail
 
-apt update && apt-upgrade
+apt update && apt-upgrade -y
 
 # This installation is for the win 10 subsystem
 # Should do no harm on real linux
-apt install \
+apt install -y \
     vim \
     zsh \
     git \
@@ -30,8 +30,6 @@ export PATH=$PATH:$GOROOT/bin
 export GOPATH=~/repositories/go
 
 # Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Install dotfiles
-echo $PWD
-ls -la
+if [[ ! -d ~/.oh-my-zsh ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
