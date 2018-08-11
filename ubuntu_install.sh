af825f35d529666474e7da78b457cf52722b5b26
Lukas Ciszewski <Lukas.Ciszewski@Gmail.com>
2018-08-11 21:58:43 +0200

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -8,17 +8,30 @@ set -o pipefail
 apt update && apt-upgrade
 
 # This installation is for the win 10 subsystem
+# Should do no harm on real linux
 apt install \
     vim \
     zsh \
     git \
     tmux \
     curl \
+    make \
+    python3 \
     golang
+
+# Setup repo struct
+mkdir -p ~/repositories/github.com/laidback
+mkdir -p ~/repositories/go/{src,pkg,bin}
+
+# Manual golang install
+tar -C /usr/local -xzf go1.10.3.linux-amd64.tar.gz
+export GOROOT=/usr/local/go
+export PATH=$PATH:$GOROOT/bin
+export GOPATH=~/repositories/go
 
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# install dotfiles
+# Install dotfiles
 echo $PWD
 ls -la
